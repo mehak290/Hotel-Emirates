7991541a2b963cf882a7a32d6788a0c95ba426d5
meghna <meghna1870.becse24@chitkara.edu.in>
2025-04-21 13:25:16 +0530

    updations in every feild

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sendBtn = document.querySelector('.send-btn');
     sendBtn.addEventListener('click', function () {
-      alert('Your response has been sent successfully! We will contact you shortly.');
+      alert('Rooms available! We will contact you shortly.');
       console.log('Send Response button clicked!');
     });
   });
