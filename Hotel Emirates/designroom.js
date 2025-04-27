a42654c6adf641324f65eae52ba1fab9de3c9f68
GeetikaChaudhary09 <geetika1155.becse24@chitkara.edu.in>
2025-04-27 19:37:40 +0530

    Set slidesPerView to 3 and spaceBetween to 20px

--- Hotel Emirates/designroom.js
+++ Hotel Emirates/designroom.js
@@ -2,8 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 20,
     loop: false,
-    centeredSlides: false,
-    slidesOffsetAfter: 0,
+    
     slidesOffsetAfter: 0,
     
         
