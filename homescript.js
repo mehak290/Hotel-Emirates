030c563fe8ed10b8ce26efc7cee4541e3f142b24
meghna <meghna1870.becse24@chitkara.edu.in>
2025-04-27 14:46:05 +0530

    message js added

--- homescript.js
+++ homescript.js
@@ -10,10 +10,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener("DOMContentLoaded", function() {
   const sendButton = document.getElementById("send-button");
+  const contactForm = document.getElementById("contact-form");
 
   sendButton.addEventListener("click", function(event) {
     event.preventDefault(); // stop the form from actually submitting yet
     alert("Thank you! Your message has been sent.");
+    console.log('contact button clicked!');
+    contactForm.reset();
   });
 });
 
