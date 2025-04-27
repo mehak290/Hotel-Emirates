58560dbc8b73c2cf10c4683e08851a0db66170e6
GeetikaChaudhary09 <geetika1155.becse24@chitkara.edu.in>
2025-04-27 19:35:05 +0530

    Update success message text to show booking confirmation

--- Hotel Emirates/designroom.js
+++ Hotel Emirates/designroom.js
@@ -10,7 +10,7 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
       },
-      breakpoints: {
+  
        
             
                
